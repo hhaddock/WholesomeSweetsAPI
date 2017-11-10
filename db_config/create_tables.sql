ed7a622535e09d9077796054c871b940706b0970
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-11-09 22:27:04 -0600

    Added foreign key constraints

--- db_config/create_tables.sql
+++ db_config/create_tables.sql
@@ -16,32 +16,41 @@ CREATE TABLE user (
 CREATE TABLE user_alias(
   fk_email VARCHAR(128) NOT NULL,
   alias VARCHAR(128),
-  FOREIGN KEY (fk_email) REFERENCES  user (email),
+  FOREIGN KEY (fk_email) REFERENCES  user (email)
+  ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY (fk_email)
 );
+
 CREATE TABLE product(
   product VARCHAR(128) NOT NULL,
   description TEXT NOT NULL,
   PRIMARY KEY (product)
 );
+
 CREATE TABLE product_price(
   fk_product VARCHAR(128) NOT NULL,
   price DECIMAL(11, 2),
-  FOREIGN KEY(fk_product) REFERENCES product (product),
+  FOREIGN KEY(fk_product) REFERENCES product (product)
+  ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY(fk_product)
 );
+
 CREATE TABLE product_picture(
   fk_product VARCHAR(128) NOT NULL,
   path_to_picture VARCHAR(128),
-  FOREIGN KEY(fk_product) REFERENCES product(product),
+  FOREIGN KEY(fk_product) REFERENCES product(product)
+  ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY(fk_product)
 );
+
 CREATE TABLE product_stock(
   fk_product VARCHAR(128) NOT NULL,
   stock INT(11),
-  FOREIGN KEY(fk_product) REFERENCES product (product),
+  FOREIGN KEY(fk_product) REFERENCES product (product)
+  ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY(fk_product)
 );
+
 CREATE TABLE `order`(
   order_num INT(11) NOT NULL AUTO_INCREMENT,
   fk_product VARCHAR(128) NOT NULL,
@@ -51,9 +60,11 @@ CREATE TABLE `order`(
   FOREIGN KEY (fk_email) REFERENCES user (email),
   PRIMARY KEY (order_num)
 );
+
 CREATE TABLE order_complete(
   fk_order_num INT(11) NOT NULL,
   status VARCHAR(128) NOT NULL,
-  FOREIGN KEY(fk_order_num) REFERENCES `order` (order_num),
+  FOREIGN KEY(fk_order_num) REFERENCES `order` (order_num)
+  ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY (fk_order_num)
 );
