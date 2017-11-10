e5dd5e022a34c007f882b71990837a218da2f986
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-11-09 21:34:44 -0600

    Added the app-user password for DB connection. This user has SELECT, INSERT, DELETE and UPDATE permissions.

--- app/connection.js
+++ app/connection.js
@@ -6,7 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'app-user',
-  password: '',
+  password: 'P@ssword69',
   database: 'bakery'
 });
 
