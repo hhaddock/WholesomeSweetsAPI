efd4dacaf1e6d4fbe1caf2c04512e8a9c07674df
Michael Douglas <michaeld1020@gmail.com>
2017-11-09 22:02:41 -0600

    Finished create table statements for DB

--- db_config/create_tables.sql
+++ db_config/create_tables.sql
@@ -9,16 +9,51 @@ DROP TABLE IF EXISTS order_complete;
 
 CREATE TABLE user (
   email VARCHAR(128) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
   PRIMARY KEY (email)
 );
 
 CREATE TABLE user_alias(
-  
+  fk_email VARCHAR(128) NOT NULL,
+  alias VARCHAR(128),
+  FOREIGN KEY (fk_email) REFERENCES  user (email),
+  PRIMARY KEY (fk_email)
 );
-CREATE TABLE product();
-CREATE TABLE product_price();
-CREATE TABLE product_picture();
-CREATE TABLE product_stock();
-CREATE TABLE order();
-CREATE TABLE order_complete();
+CREATE TABLE product(
+  product VARCHAR(128) NOT NULL,
+  description TEXT NOT NULL,
+  PRIMARY KEY (product)
+);
+CREATE TABLE product_price(
+  fk_product VARCHAR(128) NOT NULL,
+  price DECIMAL(11, 2),
+  FOREIGN KEY(fk_product) REFERENCES product (product),
+  PRIMARY KEY(fk_product)
+);
+CREATE TABLE product_picture(
+  fk_product VARCHAR(128) NOT NULL,
+  path_to_picture VARCHAR(128),
+  FOREIGN KEY(fk_product) REFERENCES product(product),
+  PRIMARY KEY(fk_product)
+);
+CREATE TABLE product_stock(
+  fk_product VARCHAR(128) NOT NULL,
+  stock INT(11),
+  FOREIGN KEY(fk_product) REFERENCES product (product),
+  PRIMARY KEY(fk_product)
+);
+CREATE TABLE `order`(
+  order_num INT NOT NULL,
+  fk_product VARCHAR(128) NOT NULL,
+  fk_email VARCHAR(128) NOT NULL,
+  quantity INT(11),
+  FOREIGN KEY (fk_product) REFERENCES product (product),
+  FOREIGN KEY (fk_email) REFERENCES user (email),
+  PRIMARY KEY (order_num)
+);
+CREATE TABLE order_complete(
+  fk_order_num VARCHAR(128) NOT NULL,
+  status VARCHAR(128) NOT NULL,
+  FOREIGN KEY(fk_order_num) REFERENCES `order` (order_num),
+  PRIMARY KEY (fk_order_num)
+);
