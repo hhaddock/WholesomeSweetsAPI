a14dd8c3a8d0928f8974a132b408a4d6b9d9d2f1
hhaddock <haydenhaddock1@gmail.com>
2017-11-09 23:00:25 -0600

    edited db conn and tested data retrieval

--- app/connection.js
+++ app/connection.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 //Create connection variable that will be used throughout the
 //api to talk to the database
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: '54.244.76.150',
   user: 'app-user',
   password: 'P@ssword69',
   database: 'bakery'
@@ -14,8 +14,9 @@ var connection = mysql.createConnection({
 //if the connection fails
 connection.connect(function(err) {
   if (err) throw err
-  console.log('Database Connection Successful!');
+  else console.log('Database Connection Successful!')
 });
-connection.query('USE bakery');
+// connection.query('USE bakery');
 
+//Returns the connection as a useable variable
 module.exports = connection;
--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../connection');
+var db = require('./connection');
 
 //This is an example route
 //We do not need to include this in prod
 router.get('/users', function(req,res){
-  // db.query('SELECT * FROM uni_branches', function(err, rows){
-  //   res.json(rows);
-  // });
+  db.query('SELECT * FROM user', function(err, rows){
+    res.json(rows);
+  });
   console.log("Test Route");
 });
 
