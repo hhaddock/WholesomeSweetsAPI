8bdef891ece05d90ed2ecd2f75f01928e0dd8bb5
David Auger <deayqf@mail.missouri.edu>
2017-11-27 17:15:14 -0600

    commit

--- app/product.js
+++ app/product.js
@@ -2,24 +2,21 @@ var express = require('express');
 var router = express.Router();
 var db = require('./connection');
 
-router.post( '/inventory', function( req, res ) {
+router.post( '/products', function( req, res ) {
     let SQL = `
         SELECT product.product, product.description, product_price.price,
                product_stock.stock, product_picture.path_to_picture
-        FROM   product, product_price, product_stock, product_picture
-        WHERE  product.product = product_price.fk_product AND
-               product.product = product_stock.fk_product AND
-               product.product = product_picture.fk_product
+        FROM   product
+        JOIN   product_price ON (product.product = product_price.fk_product)
+        JOIN   product_stock ON (product.product = product_stock.fk_product)
+        JOIN   product_picture ON (product.product = product_picture.fk_product)
     `;
 
     db.query( SQL, function( err, rows ) {
         let row_count = rows.length;
 
         if( row_count > 0 ) {
-            for( product in rows ) {
-                console.log( product );
-            }
-            res.send( "Loaded products" );
+            res.send( rows );
         }
         else {
             res.send( "Couldnt load product info" );
