d8893bbefd49c8ac58726612f8c56d7c9beb3bdb
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-11-29 16:09:22 -0600

    trying to lets cors work

--- server.js
+++ server.js
@@ -9,11 +9,27 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
+// maybe
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 //Creating an instance of of the Express server
 var app = express();
 
 //Configure body-parser to use URLENCODED and json
 //This is basic setup stuff for the server
+app.use(allowCrossDomain);//uses the above cors function
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
