47ff20a77d56bb36b5f7563fdec1ebca59f7d88f
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-11-28 21:19:28 -0600

    working multiple product orders

--- app/order.js
+++ app/order.js
@@ -12,16 +12,26 @@ router.post( '/create_order', function( req, res ) {
 function insert_order(res, body){
   // insert into order
   db.query('INSERT INTO `order` (fk_product, fk_email, quantity) VALUES (?, ?, ?)', [body.product, body.email, body.quantity], function(err, rows){
-    // get last order_group and bump one
+    // insert into order_group with next highest order_group
     db.query('SELECT order_group FROM `order_group` ORDER BY order_group DESC LIMIT 1', function(err, rows){
-      let order_group = ++rows[0].order_group;
-      // insert into order_group with next highest order_group
-      db.query('INSERT INTO order_group (fk_order_num, order_group) VALUES(LAST_INSERT_ID(), ?)', [order_group], function(err, rows){
-        // insert into order_complete
-        db.query('INSERT INTO order_complete (fk_order_group, status) VALUES (?, 0)', [order_group], function(err, rows){
-
+      // get last order_group and bump one
+      if(body.order_group == null){//if order_group isn't set it is a new order
+        let order_group = ++rows[0].order_group;
+        db.query('INSERT INTO order_group (fk_order_num, order_group) VALUES(LAST_INSERT_ID(), ?)', [order_group], function(err, rows){
+          // insert into order_complete
+          db.query('INSERT INTO order_complete (fk_order_group, status) VALUES (?, 0)', [order_group], function(err, rows){
+            res.send(String(order_group));
+          });
         });
-      });
+      } else {
+        let order_group = body.order_group;
+        db.query('INSERT INTO order_group (fk_order_num, order_group) VALUES(LAST_INSERT_ID(), ?)', [order_group], function(err, rows){
+          // insert into order_complete
+          db.query('INSERT INTO order_complete (fk_order_group, status) VALUES (?, 0)', [order_group], function(err, rows){
+            res.send(String(order_group));
+          });
+        });
+      }
     });
   });
 }
@@ -29,12 +39,11 @@ function insert_order(res, body){
 function checkAuthenticated(res, body){
   var x = 0;
   db.query('SELECT active FROM user WHERE email = ?', [body.email], function(err, rows){
-      if(rows[0].active == 1){
-        insert_order(res, body);
-        res.send("order submitted");
-      } else {
-        res.send("not logged in");
-      }
+    if(rows[0].active == 1){
+      insert_order(res, body);
+    } else {
+      res.send("not logged in");
+    }
   });
 }
 
