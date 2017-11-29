f361577a63c8d380c49fd245ce121004070bd9e8
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-11-28 21:03:04 -0600

    Working create order logic for single product

--- app/order.js
+++ app/order.js
@@ -2,54 +2,38 @@ var express = require('express');
 var router = express.Router();
 var db = require('./connection');
 
+var temp_order_group = 0;
+var temp_order_num = 0;
+
 router.post( '/create_order', function( req, res ) {
-  checkAuthenticated(req.body.email, createOrder(res, req.body));
+  checkAuthenticated(res ,req.body);
 });
 
+function insert_order(res, body){
+  // insert into order
+  db.query('INSERT INTO `order` (fk_product, fk_email, quantity) VALUES (?, ?, ?)', [body.product, body.email, body.quantity], function(err, rows){
+    // get last order_group and bump one
+    db.query('SELECT order_group FROM `order_group` ORDER BY order_group DESC LIMIT 1', function(err, rows){
+      let order_group = ++rows[0].order_group;
+      // insert into order_group with next highest order_group
+      db.query('INSERT INTO order_group (fk_order_num, order_group) VALUES(LAST_INSERT_ID(), ?)', [order_group], function(err, rows){
+        // insert into order_complete
+        db.query('INSERT INTO order_complete (fk_order_group, status) VALUES (?, 0)', [order_group], function(err, rows){
 
-// for creating we need the email, product, and quantity
-function createOrder(res, body){
-  var order_group = getNextOrderGroup();
-  insertOrder(res, body.product, body.email, body.quantity, insertOrderGroup(res, order_group, insertOrderComplete(res, order_group)));
-}//end submit order
-
-function insertOrder(res, fk_product, fk_email, quantity, callBack){
-  let SQL = 'INSERT INTO `order` (fk_product, fk_email, quantity) VALUES (?, ?, ?)';
-  db.query(SQL, [fk_product, fk_email, quantity], function(err, rows){
-    callBack;
+        });
+      });
+    });
   });
 }
 
-function getNextOrderGroup(){
-  db.query("SELECT order_group FROM order_group ORDER BY order_group ASC LIMIT 1", function(err, rows){
-    return rows[0].order_group++;
-  });
-}
-
-function insertOrderGroup(res, order_group, callBack){
-  let SQL = `
-    INSERT INTO order_group (fk_order_num, order_group) VALUES(LAST_INSERT_ID(), ?)
-  `;
-  db.query(SQL, [order_group], function(err, rows){
-    callBack;
-  });
-}
-
-function insertOrderComplete(res, fk_order_group){
-  let SQL = `
-    INSERT INTO order_complete (fk_order_group) VALUES (?)
-  `;
-  db.query(SQL, [fk_order_group], function(err, rows){
-    res.send("Order completed");
-  });
-}
-
-function checkAuthenticated(email, callBack){
+function checkAuthenticated(res, body){
   var x = 0;
-  db.query('SELECT active FROM user WHERE email = ?', [email], function(err, rows){
-      let active = rows[0].active;
-      if(active == 1){
-        callBack;
+  db.query('SELECT active FROM user WHERE email = ?', [body.email], function(err, rows){
+      if(rows[0].active == 1){
+        insert_order(res, body);
+        res.send("order submitted");
+      } else {
+        res.send("not logged in");
       }
   });
 }
